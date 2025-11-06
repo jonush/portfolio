418c80ff981bb7a2902734fb91caf0f52724fcfc
jonhsu <jonhsu.design@gmail.com>
2025-11-05 21:08:42 -0800

    Make footer stack vertically on mobile devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
 
   return (
     <footer className="border-t border-gray-700">
-      <div className="p-6 text-sm flex items-center justify-between font-mono">
-        <div className="flex items-center gap-4">
+      <div className="p-6 text-sm flex flex-col md:flex-row items-center justify-between gap-4 font-mono">
+        <div className="flex flex-col md:flex-row items-center gap-4">
           <Link
             href="https://linkedin.com/in/jonathanhsu"
             target="_blank"
